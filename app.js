e6a7e9a2f1184cf920abe6b0bd9e7712dcf4e7e5
gypsyelliot <elliot@elliotreed.net>
2020-03-14 20:59:36 -0600

    basic racer

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-var body = document.querySelector('body');
+const track = document.getElementById('track');
+const colors = ['crimson', 'green', 'blue', 'fuchsia']
+class Racer {
+	constructor(name) {
+		this.name = name;
+		this.color = colors[Math.floor(Math.random() * Math.floor(colors.length))];
+    this.display()
+	}
 
-body.innerHTML = `<h1>Racer!</h1>`;
+	display() {
+		let body = document.createElement('div')
+		body.classList.add('racer');
+		body.style.backgroundColor = this.color;
+		body.style.borderLeftColor = this.color;
+		track.append(body);
+		console.log(Math.floor(Math.random() * Math.floor(colors.length)))
+  }
+
+}
+
+const racer1 = new Racer('1');
+const racer2 = new Racer('2');
